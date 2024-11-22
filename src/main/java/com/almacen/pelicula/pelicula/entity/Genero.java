c1f03993e74a84a5719de324ae0408acb7ce4e20
Celso64 <carlosfarra15@gmail.com>
2024-11-22 16:30:14 -0300

    Fix Genero

--- src/main/java/com/almacen/pelicula/pelicula/entity/Genero.java
+++ src/main/java/com/almacen/pelicula/pelicula/entity/Genero.java
@@ -1,16 +1,22 @@
 package com.almacen.pelicula.pelicula.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Genero {
-    @OneToMany(mappedBy = "genero", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    @OneToMany(mappedBy = "genero", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     Set<Pelicula> pelicula;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
